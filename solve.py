3cecafedd4e35b256f221f4d79577cb7e21ef70b
Alex Postolache <203631992+alecripo@users.noreply.github.com>
2025-10-25 14:35:50 +0300

    fix: compute tcp checksum correctly

--- solve.py
+++ solve.py
@@ -1,3 +1,4 @@
+import operator
 
 
 # The TCP header is the 1s complement of the sum of 1s complement of:
@@ -9,14 +10,15 @@
 #   - TCP length: tcp header + tcp data
 # - the TCP header, with 0s for the checksum
 # - the TCP data, padded with 0s to the right if it is not a multiple of 16
+
 class InvalidAddress(Exception):
     def __init__(self, addr: str):
-        super.__init__(self)
         self.add_note(f"Invalid address {addr}")
 
-def ip_int_to_str(addr: int) -> str:
+
+def ip_int2str(addr: int) -> str:
     addr_str = ""
-    mask = 0xff << 24
+    mask = 0xFF << 24
     shift = 24
     for _ in range(4):
         addr_str += str((addr & mask) >> shift) + "."
@@ -24,63 +26,67 @@ def ip_int_to_str(addr: int) -> str:
         mask = mask >> 8
     return addr_str[:-1]
 
-def ip_str_to_int(addr: str) -> int:
+
+def ip_str2int(addr: str) -> int:
     parts = addr.split(".")
     if len(parts) != 4:
         raise InvalidAddress(addr)
     addr_int = 0
     shift_modifier = 0
     for part in parts:
-        addr_int += int(part) << (24-shift_modifier)
+        addr_int += int(part) << (24 - shift_modifier)
         shift_modifier += 8
     return addr_int
 
-def ip_hdr_from_addresses(src: str, dst: str, tcp_length: int) -> bytearray:
+
+def tcp_pseudo_hdr(src_ip: str, dst_ip: str, tcp_length: int) -> bytearray:
     hdr = bytearray()
 
-    hdr += ip_str_to_int(src).to_bytes(4)
-    hdr += ip_str_to_int(dst).to_bytes(4)
+    hdr += ip_str2int(src_ip).to_bytes(4)
+    hdr += ip_str2int(dst_ip).to_bytes(4)
     hdr += int(0).to_bytes()
     hdr += int(0x06).to_bytes()
     hdr += tcp_length.to_bytes(2)
 
+    assert len(hdr) * 8 == 96, f"Invalid header length: expected 94, got {len(hdr) * 8}"
     return hdr
 
+
 def checksum(ip_hdr: bytearray, tcp_segment: bytearray) -> int:
-    tcp_segment[16:18] = int(0).to_bytes(2)
-    chksum = 0
+    tcp_segment = tcp_segment[:16] + b"\x00\x00" + tcp_segment[18:]
     if len(tcp_segment) % 2 == 1:
-        tcp_segment += int(0).to_bytes()
+        tcp_segment += b"\x00"
+    chksum = 0
     data = ip_hdr + tcp_segment
-    for i in range(len(data)-1):
-        part = data[i:i+2]
-        chksum += int.from_bytes(part, "big")
-        chksum = (chksum & 0xffff) + (chksum >> 16)
-    return (~chksum) & 0xffff
+    for i in range(0, len(data) - 1, 2):
+        part = int.from_bytes(data[i : i + 2], "big")
+        chksum += part
+        chksum = (chksum & 0xFFFF) + (chksum >> 16)
+    return operator.xor(chksum, 0xFFFF)
+
 
 def main():
     num_pairs = 10
     for i in range(num_pairs):
-        with (
-            open(f"tcp_addrs_{i}.txt", "r") as f,
-            open(f"tcp_data_{i}.dat", "rb") as g
-        ):
+        with open(f"tcp_addrs_{i}.txt", "r") as f, open(f"tcp_data_{i}.dat", "rb") as g:
             addresses = f.readline().split(" ")
             if len(addresses) != 2:
                 continue
             src = addresses[0].strip()
             dst = addresses[1].strip()
             packet = bytearray(g.read())
-            actual_checksum = int.from_bytes(packet[16:18])
+            actual_checksum = int.from_bytes(packet[16:18], "big")
 
-            tcp_pseudo_header = ip_hdr_from_addresses(src, dst, len(packet))
+            tcp_pseudo_header = tcp_pseudo_hdr(src, dst, len(packet))
             comp_chksum = checksum(tcp_pseudo_header, packet.copy())
 
             if comp_chksum != actual_checksum:
-                print(f"File {i}: FAIL")
-                print(f"Computed: {comp_chksum}, actual: {int.from_bytes(packet[16:18])}")
+                print(f"File {i+1}: FAIL")
+                print(f"computed: {comp_chksum:016b}")
+                print(f"actual:   {actual_checksum:016b}")
             else:
-                print(f"File {i}: PASS")
+                print(f"File {i+1}: PASS")
+
 
 if __name__ == "__main__":
     main()
