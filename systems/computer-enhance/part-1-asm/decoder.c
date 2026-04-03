1d5dcc11343133a550433c5a0363187c786ae807
alecripo <203631992+alecripo@users.noreply.github.com>
2026-04-03 10:21:36 +0300

    feat: parse adds

--- systems/computer-enhance/part-1-asm/decoder.c
+++ systems/computer-enhance/part-1-asm/decoder.c
@@ -8,10 +8,21 @@ char* REGISTERS_SMALL[8] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
 char* REGISTERS_WIDE[8] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
 char* EFFECTIVE_ADDRESSES[8] = {"bx + si", "bx + di", "bp + si", "bp + di", "si", "di", "bp", "bx"};
 
-int
-handle_mov(int byte, FILE* infile, FILE* outfile) {
-    char dest[32]="";
-    char src[32]="";
+typedef struct operands {
+    char dest[32];
+    char src[32];
+} operands;
+
+typedef enum parse_error {
+    NO_ERROR = 0,
+    EOF_ERROR = 1,
+} parse_error;
+
+typedef parse_error (op_handler)(int byte, FILE* infile, FILE* outfile);
+
+
+parse_error
+parse_displacement_op(int byte, FILE* instruction_stream, operands* oprs) {
     bool is_first_op_dest = byte & 0b00000010;
     char** register_names;
     // Are we working with wide or small registers?
@@ -20,65 +31,64 @@ handle_mov(int byte, FILE* infile, FILE* outfile) {
     } else {
         register_names = REGISTERS_SMALL;
     }
-
     // Layout:
     // _  _              _  _  _            _  _  _
     //|2 bits : mode|   |3 bits: register| |3 bits: register/memory|
-    if ( (byte = fgetc(infile)) == EOF ) {
+    if ( (byte = fgetc(instruction_stream)) == EOF ) {
         fprintf(stderr, "Invalid byte stream: expected second byte of MOV, got EOF");
-        return 1;
+        return EOF_ERROR;
     }
     uint8_t mode = (byte >> 6);
     uint8_t reg = (byte & 0b00111000) >> 3;
     uint8_t reg_or_mem = (byte & 0b00000111);
 
-    strncpy(dest, register_names[reg], 32);
-    strncpy(src, register_names[reg_or_mem], 32);
+    strncpy(oprs->dest, register_names[reg], 32);
+    strncpy(oprs->src, register_names[reg_or_mem], 32);
 
     switch (mode) {
     case 0x00:
         if (reg_or_mem != 0b00000110) {
-            sprintf(src, "[%s]", EFFECTIVE_ADDRESSES[reg_or_mem]);
+            sprintf(oprs->src, "[%s]", EFFECTIVE_ADDRESSES[reg_or_mem]);
             break;
         }
         int16_t imm_val = 0;
-        byte = fgetc(infile);
+        byte = fgetc(instruction_stream);
         if (byte == EOF) {
             fprintf(stderr, "Invalid byte stream: expected immediate value, got EOF");
-            return 1;
+            return EOF_ERROR;
         }
         imm_val |= byte;
-        byte = fgetc(infile);
+        byte = fgetc(instruction_stream);
         if (byte == EOF) {
             fprintf(stderr, "Invalid byte stream: expected immediate value, got EOF");
-            return 1;
+            return EOF_ERROR;
         }
         imm_val = ((int16_t)byte << 8) | imm_val;
-        sprintf(src, "%d", imm_val);
+        sprintf(oprs->src, "%d", imm_val);
         break;
     // handle 8-bit and 16-bit memory displacements in the same block
     case 0x01: /* FALLTHROUGH */
     case 0x02: {
         int16_t displacement = 0;
-        if ((byte = fgetc(infile)) == EOF) {
+        if ((byte = fgetc(instruction_stream)) == EOF) {
             fprintf(stderr, "Invalid byte stream: expected 8-bit displacement, got EOF");
-            return 1;
+            return EOF_ERROR;
         }
         displacement |= byte;
 
         if (mode == 0x02) {
-            byte = fgetc(infile);
+            byte = fgetc(instruction_stream);
             if (byte == EOF) {
                 fprintf(stderr, "Invalid byte stream: expected 16-bit displacement, got EOF");
-                return 1;
+                return EOF_ERROR;
             }
             displacement |= ((int16_t)byte<<8);
         }
 
         if (displacement == 0) {
-            sprintf(src, "[%s]", EFFECTIVE_ADDRESSES[reg_or_mem]);
+            sprintf(oprs->src, "[%s]", EFFECTIVE_ADDRESSES[reg_or_mem]);
         } else {
-            sprintf(src, "[%s + %d]", EFFECTIVE_ADDRESSES[reg_or_mem], displacement);
+            sprintf(oprs->src, "[%s + %d]", EFFECTIVE_ADDRESSES[reg_or_mem], displacement);
         }
         break;
     }
@@ -89,41 +99,91 @@ handle_mov(int byte, FILE* infile, FILE* outfile) {
 
     if (!is_first_op_dest) {
         char aux[32];
-        strncpy(aux, dest, 32);
-        strncpy(dest, src, 32);
-        strncpy(src, aux, 32);
+        strncpy(aux, oprs->dest, 32);
+        strncpy(oprs->dest, oprs->src, 32);
+        strncpy(oprs->src, aux, 32);
     }
-    fprintf(outfile, "mov %s, %s\n", dest, src);
-    return 0;
-
+    return NO_ERROR;
 }
-int
-handle_immediate_mov(int op_byte, FILE* infile, FILE* outfile) {
-    bool is_wide = op_byte & 0b00001000;
-    uint8_t reg = op_byte & 0b00000111;
+
+parse_error
+parse_immediate_op(int byte, FILE* instruction_stream, operands* oprs) {
+    bool is_wide = byte & 0b00001000;
     int16_t immediate=0;
+    uint8_t reg = byte & 0b00000111;
 
-    if ( (op_byte = fgetc(infile)) == EOF ) {
-        fprintf(stderr, "Invalid byte stream: expected second byte of MOV, got EOF");
-        return 1;
+
+    if ( (byte = fgetc(instruction_stream)) == EOF ) {
+        fprintf(stderr, "Invalid byte stream: expected second byte of op, got EOF");
+        return EOF_ERROR;
     }
-    immediate |= op_byte;
+    immediate |= byte;
 
-    if (!is_wide) {
+    if (is_wide) {
+        if ( (byte = fgetc(instruction_stream)) == EOF ) {
+            fprintf(stderr, "Invalid byte stream: expected second byte of op, got EOF");
+            return EOF_ERROR;
+        }
+        immediate = ((int16_t)byte)<<8 | immediate;
+        strncpy(oprs->dest, REGISTERS_WIDE[reg], 32);
+        snprintf(oprs->src, 32, "%d", immediate);
+    }
+    else {
         immediate = (immediate ^ 0x80)-0x80;
-        fprintf(outfile, "mov %s, %d\n", REGISTERS_SMALL[reg], immediate);
-        return 0;
+        strncpy(oprs->dest, REGISTERS_SMALL[reg], 32);
+        snprintf(oprs->src, 32, "%d", immediate);
     }
 
-    if ( (op_byte = fgetc(infile)) == EOF ) {
-        fprintf(stderr, "Invalid byte stream: expected second byte of MOV, got EOF");
-        return 1;
-    }
-    int16_t high_byte = op_byte;
-    immediate = high_byte<<8 | immediate;
+    return NO_ERROR;
+}
 
-    fprintf(outfile, "mov %s, %d\n", REGISTERS_WIDE[reg], immediate);
-    return 0;
+parse_error
+handle_add(int byte, FILE* infile, FILE* outfile) {
+    operands oprs;
+    parse_error err = parse_displacement_op(byte, infile, &oprs);
+    if (err != NO_ERROR) {
+        return err;
+    }
+    fprintf(outfile, "add %s, %s\n", oprs.dest, oprs.src);
+    return NO_ERROR;
+}
+
+parse_error
+handle_mov(int byte, FILE* infile, FILE* outfile) {
+    operands oprs;
+    parse_error err = parse_displacement_op(byte, infile, &oprs);
+    if (err != NO_ERROR) {
+       return err;
+    }
+    fprintf(outfile, "mov %s, %s\n", oprs.dest, oprs.src);
+    return NO_ERROR;
+}
+
+parse_error
+handle_immediate_mov(int op_byte, FILE* infile, FILE* outfile) {
+    operands oprs;
+    parse_error err = parse_immediate_op(op_byte, infile, &oprs);
+    if (err != NO_ERROR) {
+        return err;
+    }
+    fprintf(outfile, "mov %s, %s\n", oprs.dest, oprs.src);
+    return NO_ERROR;
+}
+
+parse_error
+handle_immediate_add(int op_byte, FILE* infile, FILE* outfile) {
+    operands oprs;
+    parse_error err = parse_immediate_op(op_byte, infile, &oprs);
+    if (err != NO_ERROR) {
+        return err;
+    }
+    fprintf(outfile, "add %s, %s\n", oprs.dest, oprs.src);
+    return NO_ERROR;
+}
+
+parse_error not_implemented(int byte, FILE* infile, FILE* outfile) {
+    fprintf(stderr, "error: unknown opcode: %x\n", byte);
+    return NO_ERROR;
 }
 
 int
@@ -134,7 +194,7 @@ main(int argc, char** argv) {
             <OUTFILE>: file where output will be written. '-' writes to stdout\n");
         return 1;
     }
-    FILE* infile = fopen(argv[1], "r");
+    FILE* infile = fopen(argv[1], "rb");
     if (infile == NULL) {
         printf("Invalid input file: failed to open %s", argv[1]);
         return 1;
@@ -150,48 +210,39 @@ main(int argc, char** argv) {
         }
     }
 
-    int byte;
+    uint8_t i = 0;
+    op_handler *op_handlers[256];
+    for (i=0x0; i<0xff; i++) {
+        op_handlers[i] = not_implemented;
+    }
+    for (i=0x00; i<0x04; i++) {
+        op_handlers[i] = handle_add;
+    }
+    op_handlers[0x04] = handle_immediate_add;
+    op_handlers[0x05] = handle_immediate_add;
+    op_handlers[0x88] = handle_mov;
+    op_handlers[0x89] = handle_mov;
+    op_handlers[0x8a] = handle_mov;
+    op_handlers[0x8b] = handle_mov;
+    op_handlers[0x8c] = handle_mov;
+    op_handlers[0x8e] = handle_mov;
+    op_handlers[0xc6] = handle_mov;
+    op_handlers[0xc7] = handle_mov;
+    for (i=0xb0; i<0xc0; i++) {
+        op_handlers[i]=handle_immediate_mov;
+    }
+
+    int op_first_byte;
     loop:
     while (true) {
-        byte = fgetc(infile);
-        if (byte == EOF) {
+        op_first_byte = fgetc(infile);
+        if (op_first_byte == EOF) {
             break;
         }
-
-        /* Opcode Layout:
-            _  _  _  _           _             _ _ _
-           |4 bits : opcode>|   |1 bit: wide| |3 bits: register|
-        */
-        switch (byte >> 4) {
-            case 0b00001011:
-                bool error = handle_immediate_mov(byte, infile, outfile);
-                if (error) {
-                    return 1;
-                }
-                goto loop;
-            default:
-                break;
-        }
-
-        /*  Opcode Layout:
-            _  _  _  _  _  _     _                    _
-            |6 bits : opcode>|   |1 bit: destination| |1 bit: wide|
-        */
-        switch (byte >> 2) {
-        case 0b00100010: /* FALLTHROUGH */
-        case 0b00110001: /* FALLTHROUGH */
-        case 0b00101000: /* FALLTHROUGH */
-        case 0b00100011:
-            bool error = handle_mov(byte, infile, outfile);
-            if (error) {
-                return 1;
-            }
-            goto loop;
-        default:
-            fprintf(stderr, "error: unknown opcode: %x\n", byte>>2);
+        int err = op_handlers[(uint8_t)op_first_byte](op_first_byte, infile, outfile);
+        if (err) {
             break;
         }
-
     }
     return 0;
 }
